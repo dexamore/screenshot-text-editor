0ab098b977aac9a8b01eae9fcf4ce8ce460a78eb
dexamore <dexamore98@gmail.com>
2026-04-07 14:08:45 +0700

    Create Vite configuration file for React development

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    strictPort: true,
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: true,
+  },
+});
