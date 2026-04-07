6d20d4dacdc2cebbeb3e8745aab57e112bf67a2d
dexamore <dexamore98@gmail.com>
2026-04-07 14:13:11 +0700

    Create src/App.jsx with main application component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Screenshot Text Editor</h1>
+      {/* Add your editor component here */}
+    </div>
+  );
+};
+
+export default App;
